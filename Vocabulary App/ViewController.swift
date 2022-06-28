50a99f997d917f68523086095e1c9370fdfbb7b4
Shien <f0928652413@gmail.com>
2022-06-28 15:12:27 +0800

    Initial Commit

---
+++ Vocabulary App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vocabulary App
+//
+//  Created by Shien on 2022/6/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
